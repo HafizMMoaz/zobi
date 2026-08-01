6d0cec32bfe459eb95d5cdd7004491b2b4989f73
HafizMMoaz <hafizmoazkhalid@gmail.com>
2026-08-01 14:37:31 +0500

    feat(embedded): guest token auth and embedded dashboard support

---
+++ zobi/embedded/api.py
@@ -0,0 +1,119 @@
+import logging
+from typing import Optional
+
+from flask import Response
+from flask_appbuilder.api import expose, protect, safe
+from flask_appbuilder.hooks import before_request
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+
+from zobi import is_feature_enabled
+from zobi.commands.dashboard.embedded.exceptions import (
+    EmbeddedDashboardNotFoundError,
+)
+from zobi.constants import MODEL_API_RW_METHOD_PERMISSION_MAP, RouteMethod
+from zobi.daos.dashboard import EmbeddedDashboardDAO
+from zobi.dashboards.schemas import EmbeddedDashboardResponseSchema
+from zobi.extensions import event_logger
+from zobi.models.embedded_dashboard import EmbeddedDashboard
+from zobi.reports.logs.schemas import openapi_spec_methods_override
+from zobi.views.base_api import BaseZobiModelRestApi, statsd_metrics
+
+logger = logging.getLogger(__name__)
+
+
+class EmbeddedDashboardRestApi(BaseZobiModelRestApi):
+    datamodel = SQLAInterface(EmbeddedDashboard)
+
+    @before_request
+    def ensure_embedded_enabled(self) -> Optional[Response]:
+        if not is_feature_enabled("EMBEDDED_ZOBI"):
+            return self.response_404()
+        return None
+
+    include_route_methods = RouteMethod.GET
+    class_permission_name = "EmbeddedDashboard"
+    method_permission_name = MODEL_API_RW_METHOD_PERMISSION_MAP
+
+    resource_name = "embedded_dashboard"
+    allow_browser_login = True
+
+    openapi_spec_tag = "Embedded Dashboard"
+    openapi_spec_methods = openapi_spec_methods_override
+
+    embedded_response_schema = EmbeddedDashboardResponseSchema()
+
+    @expose("/<uuid>", methods=("GET",))
+    @protect()
+    @safe
+    @statsd_metrics
+    @event_logger.log_this_with_context(
+        action=lambda self, *args, **kwargs: f"{self.__class__.__name__}.get_embedded",
+        log_to_statsd=False,
+    )
+    # pylint: disable=arguments-differ, arguments-renamed)
+    def get(self, uuid: str) -> Response:
+        """Get the dashboard's embedded configuration.
+        ---
+        get:
+          summary: >-
+            Get the dashboard's embedded configuration this endpoint is also used
+            to embed dashboards.
+          parameters:
+          - in: path
+            schema:
+              type: string
+            name: uuid
+            description: The embedded configuration uuid
+          - in: query
+            schema:
+              type: number
+            name: uiConfig
+            description: The ui config of embedded dashboard (optional).
+          - in: query
+            schema:
+              type: boolean
+            name: show_filters
+            description: Show filters (optional).
+          - in: query
+            schema:
+              type: boolean
+            name: expand_filters
+            description: Expand filters (optional).
+          - in: query
+            schema:
+              type: string
+            name: native_filters_key
+            description: Native filters key to apply filters. (optional).
+          - in: query
+            schema:
+              type: string
+            name: permalink_key
+            description: Permalink key to apply filters. (optional).
+          responses:
+            200:
+              description: Result contains the embedded dashboard configuration
+              content:
+                application/json:
+                  schema:
+                    type: object
+                    properties:
+                      result:
+                        $ref: '#/components/schemas/EmbeddedDashboardResponseSchema'
+                text/html:
+                    schema:
+                        type: string
+            401:
+              $ref: '#/components/responses/401'
+            404:
+              $ref: '#/components/responses/404'
+            500:
+              $ref: '#/components/responses/500'
+        """
+        try:
+            embedded = EmbeddedDashboardDAO.find_by_id(uuid)
+            if not embedded:
+                raise EmbeddedDashboardNotFoundError()
+            result = self.embedded_response_schema.dump(embedded)
+            return self.response(200, result=result)
+        except EmbeddedDashboardNotFoundError:
+            return self.response_404()
---
+++ zobi/embedded/view.py
@@ -0,0 +1,79 @@
+from typing import Callable
+
+from flask import abort, current_app, request
+from flask_appbuilder import expose
+from flask_login import AnonymousUserMixin, login_user
+from flask_wtf.csrf import same_origin
+
+from zobi import event_logger, is_feature_enabled
+from zobi.daos.dashboard import EmbeddedDashboardDAO
+from zobi.zobi_typing import FlaskResponse
+from zobi.utils import json
+from zobi.views.base import BaseZobiView, common_bootstrap_payload
+
+
+class EmbeddedView(BaseZobiView):
+    """The views for embedded resources to be rendered in an iframe"""
+
+    route_base = "/embedded"
+
+    @expose("/<uuid>")
+    @event_logger.log_this_with_extra_payload
+    def embedded(
+        self,
+        uuid: str,
+        add_extra_log_payload: Callable[..., None] = lambda **kwargs: None,
+    ) -> FlaskResponse:
+        """
+        Server side rendering for the embedded dashboard page
+        :param uuid: identifier for embedded dashboard
+        :param add_extra_log_payload: added by `log_this_with_manual_updates`, set a
+            default value to appease pylint
+        """
+        if not is_feature_enabled("EMBEDDED_ZOBI"):
+            abort(404)
+
+        embedded = EmbeddedDashboardDAO.find_by_id(uuid)
+
+        if not embedded:
+            abort(404)
+
+        assert embedded is not None
+
+        # validate request referrer in allowed domains
+        is_referrer_allowed = not embedded.allowed_domains
+        for domain in embedded.allowed_domains:
+            if same_origin(request.referrer, domain):
+                is_referrer_allowed = True
+                break
+
+        if not is_referrer_allowed:
+            abort(403)
+
+        # Log in as an anonymous user, just for this view.
+        # This view needs to be visible to all users,
+        # and building the page fails if g.user and/or ctx.user aren't present.
+        login_user(AnonymousUserMixin(), force=True)
+
+        add_extra_log_payload(
+            embedded_dashboard_id=uuid,
+            dashboard_version="v2",
+        )
+
+        bootstrap_data = {
+            "config": {
+                "GUEST_TOKEN_HEADER_NAME": current_app.config["GUEST_TOKEN_HEADER_NAME"]
+            },
+            "common": common_bootstrap_payload(),
+            "embedded": {
+                "dashboard_id": embedded.dashboard_id,
+            },
+        }
+
+        return self.render_template(
+            "zobi/spa.html",
+            entry="embedded",
+            bootstrap_data=json.dumps(
+                bootstrap_data, default=json.pessimistic_json_iso_dttm_ser
+            ),
+        )
